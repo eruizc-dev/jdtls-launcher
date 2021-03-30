bcbf902f364ea89b18419ff6d1ba4c4dd28d0a74
Emiliano Ruiz Carletti <contact@eruizc.dev>
2021-03-30 19:00:08 -0300

    Improve err checking

--- jdtls-launcher.sh
+++ jdtls-launcher.sh
@@ -1,47 +1,48 @@
 #!/bin/bash
 
 # ===== CHECK JAVA_HOME EXISTS ====
-if ! [[ "$JAVA_HOME" ]]; then
-    echo "JAVA_HOME not defined" >> /dev/stderr
+if ! [[ -d "$JAVA_HOME" ]]; then
+    echo "ERROR: JAVA_HOME is not defined" >> /dev/stderr
     exit 1
 fi
 
-# ===== FIND JDTLS_ROOT =====
+# ===== FIND JDTLS =====
 JDTLS_ROOT="$JAVA_HOME/../jdtls"
+EQUINOX_LAUNCHER=`find $JDTLS_ROOT/plugins -nowarn -type f -name 'org.eclipse.equinox.launcher_*' 2> /dev/null`
 
-if ! [ -d "$JDTLS_ROOT" ]; then
-    mkdir -p "$JDTLS_ROOT"
-    cd "$JDTLS_ROOT"
+if ! [[ -f "$EQUINOX_LAUNCHER" ]]; then
     echo 'WARNING: JDTLS is not installed' >> /dev/stderr
-    LATEST=`curl http://download.eclipse.org/jdtls/snapshots/latest.txt`
-    echo "INFO: About to install $LATEST"
+
+    LATEST=`curl -s http://download.eclipse.org/jdtls/snapshots/latest.txt`
+    echo "INFO: About to install $LATEST to $JDTLS_ROOT"
+
+    mkdir -p "$JDTLS_ROOT" || exit 1
+    cd "$JDTLS_ROOT"
+
     curl "http://download.eclipse.org/jdtls/snapshots/$LATEST" > "$LATEST"
     tar -xf "$LATEST"
     rm "$LATEST"
-fi
+    chmod 777 "$JDTLS_ROOT"
+    chmod -R 777 "$JDTLS_ROOT/config_*"
 
-# ===== FIND EQUINOX LAUNCHER =====
-EQUINOX_LAUNCHER="$JDTLS_ROOT/plugins/org.eclipse.equinox.launcher_*.jar"
-
-if ! [[ "$EQUINOX_LAUNCHER" ]]; then
-    echo 'ERROR: JDTLS installation failure'
-    exit 1
-else
-    echo 'INFO: `JDTLS installation succesfull'
+    EQUINOX_LAUNCHER=`find $JDTLS_ROOT/plugins -nowarn -type f -name 'org.eclipse.equinox.launcher_*' 2> /dev/null`
+    if ! [[ -f "$EQUINOX_LAUNCHER" ]]; then
+        echo 'ERROR: JDTLS installation failure' > /dev/stderr
+        exit 1
+    else
+        echo 'INFO: JDTLS installation succesfull'
+    fi
 fi
 
 # ===== FIND LOMBOK =====
-LOMBOK_PATH="$JDTLS_ROOT/plugins"
+LOMBOK="$JDTLS_ROOT/plugins/lombok.jar"
+if ! [[ -f "$LOMBOK" ]]; then
+    echo 'WARNING: Lombok is not installed' > /dev/stderr
 
-LOMBOK=`find "$LOMBOK_PATH" -type f -name 'lombok*.jar'`
-if ! [[ "$LOMBOK" ]]; then
-    echo 'WARNING: Lombok is not installed.'
-    echo "INFO: Installing Lombok to $LOMBOK_PATH"
-    curl "https://projectlombok.org/downloads/lombok.jar" > "$LOMBOK_PATH/lombok.jar"
+    echo "INFO: Installing Lombok to $LOMBOK"
+    curl "https://projectlombok.org/downloads/lombok.jar" > "$LOMBOK"
 
-    # ===== RECHECK =====
-    LOMBOK=`find "$LOMBOK_PATH" -type f -name 'lombok*.jar'`
-    if ! [[ "$LOMBOK" ]]; then
+    if ! [[ -f "$LOMBOK" ]]; then
         echo 'ERROR: Lombok installation failure' > /dev/stderr
         exit 1
     else
@@ -62,13 +63,21 @@ case "$SYSTEM" in
         ;;
     *)
         echo "ERROR: Unknown or unsupported system $SYSTEM" >> /dev/stderr
-        exit
+        echo "Consider opening a new issue: https://github.com/eruizc-dev/jdtls-launcher/issues/new?title=Unsupported%20system%20$SYSTEM" >> /dev/stderr
+        exit 1
         ;;
 esac
 
-# ===== RUN =====
+# ===== FIND JAVA EXEC  =====
+if [[ -f "$JAVA_HOME/bin/java" ]]; then
+    JAVA_EXEC="$JAVA_HOME/bin/java"
+elif [[ -f "$JAVA_HOME/bin/java.exe" ]]; then
+    JAVA_EXEC="$JAVA_HOME/bin/java.exe"
+else
+    echo 'ERROR: could not find java binary' > /dev/stderr
+fi
 
-"$JAVA_HOME/bin/java" \
+"$JAVA_EXEC" \
     -Declipse.application=org.eclipse.jdt.ls.core.id1 \
     -Dosgi.bundles.defaultStartLevel=4 \
     -Declipse.product=org.eclipse.jdt.ls.core.product \
@@ -83,3 +92,4 @@ esac
     --add-modules=ALL-SYSTEM \
     --add-opens java.base/java.util=ALL-UNNAMED \
     --add-opens java.base/java.lang=ALL-UNNAMED
+

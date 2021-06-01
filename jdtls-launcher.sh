a8419deaeb16b96f49c4d040784753fa162237d9
Emiliano Ruiz Carletti <contact@eruizc.dev>
2021-06-01 20:26:00 -0300

    Refactor run function

--- jdtls-launcher.sh
+++ jdtls-launcher.sh
@@ -30,6 +30,40 @@ function print_help {
     echo '  -h | --help         prints this menu'
 }
 
+function run {
+    SYSTEM=`uname -s`
+    case "$SYSTEM" in
+        [Ll]inux) # Linux and WSL
+            CONFIG="$JDTLS_ROOT/config_linux"
+            ;;
+        [Dd]arwin) # MacOS
+            CONFIG="$JDTLS_ROOT/config_mac"
+            ;;
+        *)
+            echo "ERROR: Unknown or unsupported system $SYSTEM" >> /dev/stderr
+            echo "Consider opening a new issue: https://github.com/eruizc-dev/jdtls-launcher/issues/new?title=Unsupported%20system%20$SYSTEM" >> /dev/stderr
+            exit 1
+            ;;
+    esac
+
+    java \
+        -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044 \
+        -Declipse.application=org.eclipse.jdt.ls.core.id1 \
+        -Dosgi.bundles.defaultStartLevel=4 \
+        -Declipse.product=org.eclipse.jdt.ls.core.product \
+        -Dlog.level=ALL \
+        -noverify \
+        -Xmx1G \
+        -javaagent:"$LOMBOK" \
+        -jar "$EQUINOX_LAUNCHER" \
+        -configuration "$CONFIG" \
+        -data "$WORKSPACE" \
+        --add-modules=ALL-SYSTEM \
+        --add-opens java.base/java.util=ALL-UNNAMED \
+        --add-opens java.base/java.lang=ALL-UNNAMED
+}
+
+### PARAM PARSING ###
 case "$1" in
     -v|--version)
         print_version
@@ -39,40 +73,12 @@ case "$1" in
         print_help
         exit 0
         ;;
+    "")
+        run
+        ;;
     *)
         echo "unknown option $1"
         print_help
         exit 0
         ;;
 esac
-
-SYSTEM=`uname -s`
-case "$SYSTEM" in
-    [Ll]inux) # Linux and WSL
-        CONFIG="$JDTLS_ROOT/config_linux"
-        ;;
-    [Dd]arwin) # MacOS
-        CONFIG="$JDTLS_ROOT/config_mac"
-        ;;
-    *)
-        echo "ERROR: Unknown or unsupported system $SYSTEM" >> /dev/stderr
-        echo "Consider opening a new issue: https://github.com/eruizc-dev/jdtls-launcher/issues/new?title=Unsupported%20system%20$SYSTEM" >> /dev/stderr
-        exit 1
-        ;;
-esac
-
-java \
-    -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044 \
-    -Declipse.application=org.eclipse.jdt.ls.core.id1 \
-    -Dosgi.bundles.defaultStartLevel=4 \
-    -Declipse.product=org.eclipse.jdt.ls.core.product \
-    -Dlog.level=ALL \
-    -noverify \
-    -Xmx1G \
-    -javaagent:"$LOMBOK" \
-    -jar "$EQUINOX_LAUNCHER" \
-    -configuration "$CONFIG" \
-    -data "$WORKSPACE" \
-    --add-modules=ALL-SYSTEM \
-    --add-opens java.base/java.util=ALL-UNNAMED \
-    --add-opens java.base/java.lang=ALL-UNNAMED

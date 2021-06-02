df8240a52445ef84fd1ad6200f034def377a4985
Emiliano Ruiz Carletti <contact@eruizc.dev>
2021-06-01 21:03:09 -0300

    Update script variables

--- jdtls-launcher.sh
+++ jdtls-launcher.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-### SCRIPT INFO ###
-SCRIPT_VERSION='v1.1.0-alpha'
+SYSTEM=`uname -s`
 
-JDTLS_ROOT="/usr/local/lib/jdtls-launcher/jdtls"
-EQUINOX_LAUNCHER=`find "$JDTLS_ROOT/plugins" -type f -name 'org.eclipse.equinox.launcher_*'`
+SCRIPT_VERSION='v1.1.0-alpha'
+SCRIPT_ROOT=`dirname $(realpath "$0")`
+
+JDTLS_ROOT="$SCRIPT_ROOT/jdtls"
+JDTLS_WORKSPACE="$HOME/.cache/jdtls-workspace"
 JDTLS_CORE=`find "$JDTLS_ROOT/plugins" -type f -name 'org.eclipse.jdt.ls.core_*'`
+JDTLS_EQUINOX_LAUNCHER=`find "$JDTLS_ROOT/plugins" -type f -name 'org.eclipse.equinox.launcher_*'`
+
 LOMBOK="$JDTLS_ROOT/plugins/lombok.jar"
-WORKSPACE="$HOME/workspace"
 
 ### UTILS ###
-function get_version {
+function get_jar_version {
     echo $(basename "$1" | awk '{ gsub(/^[^-_]*[-_]?|\.jar$/, ""); print }' | awk '{ gsub(/^$/, "custom"); print }')
 }
 
 ### ACTIONS ###
 function print_version {
     echo "jdtls-launcher $SCRIPT_VERSION"
-    echo "equinox-launcher $(get_version "$EQUINOX_LAUNCHER")"
-    echo "jdtls-core $(get_version "$JDTLS_CORE")"
-    echo "lombok $(get_version "$LOMBOK")"
+    echo "equinox-launcher $(get_jar_version "$JDTLS_EQUINOX_LAUNCHER")"
+    echo "jdtls-core $(get_jar_version "$JDTLS_CORE")"
+    echo "lombok $(get_jar_version "$LOMBOK")"
     java --version
 }
 
@@ -31,7 +34,6 @@ function print_help {
 }
 
 function run {
-    SYSTEM=`uname -s`
     case "$SYSTEM" in
         [Ll]inux) # Linux and WSL
             CONFIG="$JDTLS_ROOT/config_linux"
@@ -55,9 +57,9 @@ function run {
         -noverify \
         -Xmx1G \
         -javaagent:"$LOMBOK" \
-        -jar "$EQUINOX_LAUNCHER" \
+        -jar "$JDTLS_EQUINOX_LAUNCHER" \
         -configuration "$CONFIG" \
-        -data "$WORKSPACE" \
+        -data "$JDTLS_WORKSPACE" \
         --add-modules=ALL-SYSTEM \
         --add-opens java.base/java.util=ALL-UNNAMED \
         --add-opens java.base/java.lang=ALL-UNNAMED

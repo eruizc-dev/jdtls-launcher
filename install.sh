145f44101fd62759621bfb66ceee5f6cfc532bac
Emiliano Ruiz Carletti <contact@eruizc.dev>
2021-07-17 19:01:52 -0300

    Move default install location to ~/.local/opt

--- install.sh
+++ install.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
 
-### GATHER SYSTEM INFORMATION ###
-SYSTEM=`uname -s`
-if [ "$SYSTEM" == 'Darwin' ]; then
-    if  [ "$EUID" -eq 0 ]; then
-        echo 'ERROR: sudo is not necessary for a MACOS installation' >> /dev/stderr
-        exit 1
-    fi
-    echo 'INFO: Performing system level installation'
-elif [ "$SYSTEM" == 'Linux' ]; then
-    if  [ "$EUID" -eq 0 ]; then
-        echo 'INFO: Performing system level installation'
-    else
-        echo 'INFO: Performing user level installation'
-    fi
-else
-    echo "ERROR: $SYSTEM not supported" >> /dev/stderr
-    exit 1
-fi
-
-INSTALL_LOCATION=${1:-'/usr/local/lib'}'/jdtls-launcher'
-LINK_LOCATION='/usr/local/bin/jdtls'
-
-INSTALL_ROOT=${INSTALL_LOCATION%/*}
+### CONFIGURABLE VARIABLES ###
+INSTALL_ROOT=${1:-"$HOME/.local/opt"}
+LINK_ROOT="$HOME/.local/bin"
 TARBALL_LOCATION='/tmp/jdtls-launcher.tar.gz'
-DOWNLOAD_URL=`curl -s 'https://api.github.com/repos/eruizc-dev/jdtls-launcher/releases/latest' | grep 'tarball_url' | egrep -o 'https://[^"]+'`
+DOWNLOAD_URL='https://github.com/eruizc-dev/jdtls-launcher/archive/refs/heads/master.tar.gz'
 
-### CHECK PERMISSIONS ###
+### DERIVATED VARIABLES ###
+INSTALL_LOCATION="$INSTALL_ROOT/jdtls-launcher"
+LINK_LOCATION="$LINK_ROOT/jdtls"
+
+### PREQUISITES ###
+mkdir -p $INSTALL_ROOT
+mkdir -p $LINK_ROOT
+
+### CHECKS ###
 if [ ! -d "$INSTALL_ROOT" ]; then
-    echo "ERROR: $INSTALL_ROOT not a directory" >> /dev/stderr
+    echo "ERROR: could not create directory $INSTALL_ROOT" >> /dev/stderr
     exit 1
 fi
 
@@ -37,7 +25,6 @@ if [ ! -w "$INSTALL_ROOT" ]; then
     exit 1
 fi
 
-### CHECK EXISTING INSTALLATION ###
 if [ -d "$INSTALL_LOCATION" ]; then
     echo "ERROR: $INSTALL_LOCATION already exists, delete it manually and try again " >> /dev/stderr
     exit 1
@@ -49,16 +36,18 @@ curl -fLo "$TARBALL_LOCATION" "$DOWNLOAD_URL"
 
 echo 'INFO: Extracting JDTLS-LAUNCHER'
 tar -xf "$TARBALL_LOCATION" --directory "$INSTALL_ROOT"
-mv "$INSTALL_ROOT"/eruizc-dev-jdtls-launcher* "$INSTALL_LOCATION"
+mv "$INSTALL_ROOT/jdtls-launcher-master" "$INSTALL_LOCATION"
 chmod -R 755 "$INSTALL_LOCATION"
 
 ### CREATE LINK ###
-if [ "$INSTALL_LOCATION" == '/usr/local/lib/jdtls-launcher' ]; then
-    echo "INFO: Creating symlink ${LINK_LOCATION}"
-    rm "$LINK_LOCATION" 2> /dev/null
-    ln -s "$INSTALL_LOCATION/jdtls-launcher.sh" "$LINK_LOCATION"
-fi
+echo "INFO: Creating symlink at ${LINK_LOCATION}"
+rm "$LINK_LOCATION" 2> /dev/null
+ln -s "$INSTALL_LOCATION/jdtls-launcher.sh" "$LINK_LOCATION"
 
 ### INSTALL JDTLS ###
-jdtls --install
+$LINK_LOCATION --install
+
+### NOTIFY SUCCESS ###
+echo 'INFO: Installation successful'
+echo "INFO: Ensure $LINK_ROOT is in path"
 
